02c0a068adfe9e915da43872a7af4015ffd73248
Rio0628 <mariomdomenech@gmail.com>
2021-10-22 15:17:01 -0400

    Gave the initial style of the editEvent comp
    - Gave the initial style of the editEvent components and its initial size.
    - Added a container for the date and the type input in order to have them within the same line. Otherwise they would take too much space.

--- src/components/EditEvent.js
+++ src/components/EditEvent.js
@@ -5,13 +5,14 @@ const EditEvent = () => {
         <form className='editEventContainer'>
             <h3 className='editEventHeading'>Edit Event</h3>
             <input type='text' placeholder='Event Name...' className='nameInput' />
+            <div className='date-typeContainer'>
             <input type='date' className='dateInput'/>
-            <select className='typeInput'>
-                <option>option 1</option>
-                <option>option 2</option>
-                <option>option 3</option>
-            </select>
-
+                <select className='typeInput'>
+                    <option>option 1</option>
+                    <option>option 2</option>
+                    <option>option 3</option>
+                </select>
+            </div>
             <ul className='cancelConfirmBtns'>
                 <li className='confirmBtn'>Confirm</li>
                 <li className='cancelBtn'>Cancel</li>
