dcb1c13c23d20979b75548a744360e597ac6cdf8
Rio0628 <mariomdomenech@gmail.com>
2021-10-23 23:38:58 -0400

    Created the Add event button and its open view
    - Created the add event button and add event open view.
    - The add event open  view will trigger once the addEventTriggered state is set to true and return to the button view once it is set to false.
    - Gave both of the elements their initial position and styling.
    - Changed the App component from a function component to a class component.

--- src/App.js
+++ src/App.js
@@ -3,70 +3,114 @@ import IndEvent from './components/IndEvent';
 import EditEvent from './components/EditEvent';
 import Day from './components/Day';
 
-function App() {
-  return (
-    <div className="container">
-      <div className='eventsView'>
-        <h2 className='eventsViewHeading'>Upcoming Events</h2>
-        <div className='addEventBtn'>Add Event</div>
-        <div className='indEventCntr'>
-          <IndEvent />
-          <IndEvent />
-          <IndEvent />
-          <IndEvent />
-          <IndEvent />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addEventTriggered: false,
+    }
+  }
+
+  render () {
+
+    const onClick = (e) => {
+      console.log(e.target)
+
+      if (e.target.className === 'addEventBtn' || e.target.className === 'cnclBtnAdd') {
+        this.setState({ addEventTriggered: !this.state.addEventTriggered });
+      }
+
+      if (e.target.className === 'cnfrmBtnAdd') {
+        this.setState({ addEventTriggered: false })
+      }
+    }
+   
+    return (
+      <div className="container">
+        <div className='eventsView'>
+          <h2 className='eventsViewHeading'>Upcoming Events</h2>
+          
+          {this.state.addEventTriggered ?
+            <form className='addEventOpen'>
+              <p className='addEventOpenHeading'>Add Event</p>
+              <input type='text' placeholder='Event Name...' className='nameAddEvent' />
+              <div className='date-typeCntrAdd'>
+                <input type='date' className='dateAddEvent'/>
+                <select className='typeAddEvent'>
+                  <option>option 1</option>
+                  <option>option 2</option>
+                  <option>option 3</option>
+                </select>
+              </div>
+              <ul className='cnclCnfrmBtns'>
+                  <li className='cnfrmBtnAdd' onClick={onClick}>Confirm</li>
+                  <li className='cnclBtnAdd' onClick={onClick}>Cancel</li>
+              </ul>
+            </form>
+            : 
+            <div className='addEventBtn' onClick={onClick}>Add Event</div>
+          }
+          
+          
+          
+          
+          <div className='indEventCntr'>
+            <IndEvent />
+            <IndEvent />
+            <IndEvent />
+            <IndEvent />
+            <IndEvent />
+          </div>
         </div>
+        <div className='calendarView'>
+          <div className='month-yearHeadingCntr'>
+            <h2 className='monthHeading'>June</h2>
+            <h2 className='yearHeading'>2021</h2>
+          </div>
+          <input className='setDateInput' type='date'/>
+          <div className='mainCalendarCntr'>
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+          </div>
+        </div>
+        {/* <EditEvent /> */}
+       
       </div>
-      <div className='calendarView'>
-        <div className='month-yearHeadingCntr'>
-          <h2 className='monthHeading'>June</h2>
-          <h2 className='yearHeading'>2021</h2>
-        </div>
-        <input className='setDateInput' type='date'/>
-        <div className='mainCalendarCntr'>
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-        </div>
-      </div>
-      
-     
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
-{/* 
-      <EditEvent />  */}
