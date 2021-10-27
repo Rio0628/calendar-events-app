1aafce3d19d54a0e6585470121f5146534b740a0
Rio0628 <mariomdomenech@gmail.com>
2021-10-26 23:02:36 -0400

    Gave the Day components all of their info
    - Gave the Day components all of the individual events that have the same date as the Day component.
    - Created if conditionals to allow for the creation of IndDailyEvent components only if there are events that have a matching date.
    - Two events will show completely and the rest of events will be shown by a numeral. The numeral itself gets the length of the dailyEvents object and substracts two to get the real remaining events for the day.

--- src/components/day.js
+++ src/components/day.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import IndDailyEvent from './IndDailyEvent';
 
 const Day = (props) => {
+    let indDailyEventsCntr = [], dailyEvents = [], eventsPresent = false, eventsCountBool = false, eventsCount = 0;
+    // let [dailyEvents , setDailyEvents] = useState([]);
     // console.log(props.infoDay)
     const monthNum = (month) => {
         return month === 'Jan' ? '01' : month === 'Feb' ? '02' : month === 'Mar' ? '03' : month === 'Apr' ? '04' : month === 'May' ? '05' : month === 'Jun' ? '06' : month === 'Jul' ? '07' : month === 'Aug' ? '08' : month === 'Sep' ? '09' : month === 'Oct' ? '10' : month === 'Nov' ? '11' : month === 'Dec' ? '12' :  null;
     };
 
-    
     let date = `${props.infoDay.year}-${parseInt(monthNum(props.infoDay.month))}-${props.infoDay.dayNum}`;
-    // console.log(date)
+    
+
+    const setEvents = () => {
+        let events = props.events.filter(event => event.date === date); 
+        if (events.length > 0) {
+            dailyEvents.push(events);
+            eventsPresent = true;
+        }        
+    };
+    setEvents();
+    // console.log(eventsPresent)
+   
+    if (eventsPresent) {
+        if (dailyEvents[0].length > 1) {
+            for (let i = 0; i < 2 ; i++) {
+                indDailyEventsCntr.push( <IndDailyEvent key={'dailyEvent ' + i} /> );
+            }
+        }
+        else {
+            for (let i = 0; i < 1 ; i++) {
+                indDailyEventsCntr.push( <IndDailyEvent key={'dailyEvent ' + i} /> );
+            }
+        }
+        
+        
+        if (dailyEvents[0].length > 2) {
+            eventsCount = dailyEvents[0].length - 2;
+            eventsCountBool = true; 
+        }
+    }
+    
+    console.log(eventsCount)
+
+
     return (
         <div className='indDayView'>
             <div className='name-numDayCntr'>
                 <p className='dayName'>{props.infoDay.name}</p>
                 <p className='dayNum'>{props.infoDay.dayNum}</p>
             </div>
-            <IndDailyEvent />
-            <IndDailyEvent />
-            <p className='extraEvents'>+ 3</p>
+            {indDailyEventsCntr}
+
+            {eventsPresent && eventsCountBool ? <p className='extraEvents'>+ {eventsCount}</p>  : null }
+            
         </div>
     )
 }
